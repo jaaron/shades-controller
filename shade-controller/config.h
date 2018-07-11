9c091751855e3b82303a7f5d0acd24b73fafbb0d
J. Aaron Pendergrass <aaron@13th-floor.org>
2018-07-10 23:58:26 -0400

    modify shade controller arduino sketch to use mqtt for server interaction.

--- shade-controller/config.h
+++ shade-controller/config.h
@@ -5,13 +5,14 @@
 const char ssid[] = "MySSID";
 const char wifi_pass[] = "MyWifiPassword";
 
-/* Replace with IP of host running shade-server software 
- * and port number for raw HTTP. 
- *
- * *NB* Do not expost raw HTTP port to the internet
+/* Replace with IP and port of MQTT Broker
  */
-const char control_host[] = "192.168.1.90";
-const short control_http_port = 8080;
+const char mqtt_server[] = "192.168.1.90";
+const short mqtt_port = 1883;
+
+/* don't edit these, they must agree with the topics used by the server. */
+const char *status_topic = "shade_status";
+const char *registration_topic = "registration";
 
 /* Device ID, Name, and Description. These are used by the local
    control panel and passed to Amazon for Alexa integration. */
--- shade-controller/shade-controller.ino
+++ shade-controller/shade-controller.ino
@@ -1,6 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiUdp.h>
+
+#include <Adafruit_MQTT_Client.h>
+#include <Adafruit_MQTT.h>
 
 #define UP FORWARD
 #define DOWN BACKWARD
@@ -22,17 +24,51 @@
 */
 #include "config.h"
 
-WiFiUDP Udp;
 unsigned int localUdpPort = 4210;
 char incomingPacket[255];
 
-char buffer[16];
+char buffer[128];
 
-long dist_max = 12;
-long dist_min = 2;
+long dist_max = 100;
+long dist_min = 0;
 long distance = 0;
 long target = 0;
 long direction = 0; /* 0 for stop, -1 for reverse, +1 for forward */
+
+
+
+// MQTT stuff
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, mqtt_server, mqtt_port);
+Adafruit_MQTT_Subscribe cmdfeed = Adafruit_MQTT_Subscribe(&mqtt, id, MQTT_QOS_0);
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 10 seconds...");
+       mqtt.disconnect();
+       delay(10000);  // wait 10 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
+/* End MQTT Stuff */
+
 
 #ifdef ADAFRUIT_MOTORSHIELD 
 
@@ -99,6 +135,20 @@ void motor_stop(void){
 
 #endif
 
+#ifdef DISTANCE_TEST
+
+void distance_init(void){}
+
+long get_distance(void){
+    if(distance < target){
+	return distance +1;
+    }else if(distance > target){
+	return distance - 1;
+    }
+    return distance;
+}
+
+#else
 #ifdef DISTANCE_HC_SR05
 
 #include "SR04.h"
@@ -127,38 +177,44 @@ void distance_init(void){
   us100.begin(9600);
 }
 
-long get_distance(void){
-  int retry = 0;
-  us100.flush();
-  us100.write(0x55);
-  while(retry < 10 && us100.available() < 2){
-      retry++;
-      delay(20);
-  }
-  if(us100.available() < 2){
-      return -1;
-  }
-  unsigned short high = us100.read() << 8;
-  unsigned short low = us100.read();
-  long dist = ((long)(high + low))/10;
-  if(dist > 1000){
-      return -1;
-  }
-  return dist;
+long get_distance(void){    
+    int retry = 0;
+    us100.flush();
+    us100.write(0x55);
+    while(retry < 10 && us100.available() < 2){
+	retry++;
+	delay(20);
+    }
+    if(us100.available() < 2){
+	return -1;
+    }
+    unsigned short high = us100.read() << 8;
+    unsigned short low = us100.read();
+    long dist = ((long)(high + low))/10;
+    if(dist > 1000){
+	return -1;
+    }else if(dist < 0){
+	return -1;
+    }
+    return dist;
 }
 
+#endif
 #endif
 
 HTTPClient http_client;
 
 void register_with_server(void){
   String req = (String("{\"id\": \"")+id+"\", \"name\": \""+name+"\", \"description\": \""+description+"\", "+
-		"\"ip\": \""+WiFi.localIP().toString()+"\", \"port\": "+localUdpPort+"}\r\n");
-  Serial.println(String("Registering with server ")+control_host);  
+		"\"ip\": \""+WiFi.localIP().toString()+"\"}\r\n");
+  Serial.println(String("Registering with server ")+mqtt_server);  
+  mqtt.publish(registration_topic, req.c_str(), 0);
+  /*
   http_client.begin(control_host, control_http_port, "/register");
   http_client.addHeader("Content-type", "application/json");
   int res = http_client.POST(req);
   Serial.println(String("Got response: ") + res);
+  */
   
 }
 
@@ -191,6 +247,19 @@ void calibrate(void){
   Serial.printf("Calibrated: dist_max = %ld\n", dist_max);
 }
 
+void cmd_callback(char *data, uint16_t len){
+    Serial.printf("Got command \"%s\"\n", data);
+    if(strncmp("SET ", data, 4) == 0){
+	long tmp = strtol(data + 4, NULL, 10);
+	if(tmp >= 0 && tmp <= 100){
+	    target = percent_to_distance(tmp);
+	    Serial.printf("Set target to %ld\n", target);
+	}
+    }else if(strncmp("CALIBRATE", data, 9) == 0){
+	calibrate();
+    }
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -215,10 +284,10 @@ void setup() {
   motor_init();
   distance_init();
   
-  calibrate();
+  cmdfeed.setCallback(cmd_callback);
+  mqtt.subscribe(&cmdfeed);
 
-  Udp.begin(localUdpPort);
-
+  MQTT_connect();
   register_with_server();
 }
 
@@ -231,6 +300,9 @@ long percent_to_distance(long percent){
 }
 
 void loop() {
+    MQTT_connect();
+    mqtt.processPackets(100);
+    
     distance = get_distance();
     Serial.printf("Current distance: %ld target: %d direction: %d\n", distance, target, direction);
     
@@ -242,7 +314,7 @@ void loop() {
 	    target = distance;
 	}
     }
-
+    /*
     http_client.begin(control_host, control_http_port, String("/shade/")+id+"/poll");
     int res = http_client.POST(String(distance_to_percent()));
     if(res != 200){
@@ -267,6 +339,12 @@ void loop() {
 	    }
 	}
     }
+    */
+
+    sprintf(buffer, "{\"shade_id\": \"%s\", \"status\": %d}", id,
+	    distance_to_percent());
+    
+    mqtt.publish(status_topic, buffer, 0);
     
     if(direction == 0){
 	delay(3000);
